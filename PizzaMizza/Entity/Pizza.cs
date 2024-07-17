1fa776ed949e61469e96d7557a16c159e0f9d9fd
Yusif70 <yusifpirquliyev7@gmail.com>
2024-07-17 14:28:45 +0400

    initial commit

---
+++ PizzaMizza/Entity/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using PizzaMizza.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMizza.Entity
+{
+	public partial class Pizza(int id, string name, double price, string size, List<string> ingredients)
+	{
+		public int Id { get; set; } = id;
+		public string Name { get; set; } = name;
+		public double Price { get; set; } = price;
+		public string Size { get; set; } = size;
+	}
+	public partial class Pizza
+	{
+		public List<string> Ingredients { get; set; } = ingredients;
+	}
+}
---
+++ PizzaMizza/Services/PizzaService.cs
@@ -0,0 +1,22 @@
+﻿using PizzaMizza.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMizza.Services
+{
+	public class PizzaService
+	{
+		private List<Pizza> pizzaList = [];
+		public void CreatePizza(Pizza pizza)
+		{
+			pizzaList.Add(pizza);
+		}
+		public List<Pizza> GetPizzas()
+		{
+			return pizzaList;
+		}
+	}
+}
